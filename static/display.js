2177af29cd73d5897eb10dd4d3761a046cb2f0d1
Isaac Avram <avrisaac555@gmail.com>
2019-03-26 15:38:12 -0700

    Added Color class

--- static/display.js
+++ static/display.js
@@ -1,4 +1,30 @@
 
+class Color {
+    constructor(r, g, b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    get percievedBrightness() {
+        return 0.299 * this.r + 0.578 * this.g + 0.114 * this.b;
+    }
+}
+
+function colorFromHex(hexCode) {
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hexCode = hexCode.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? new Color(
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+    ) : null;
+}
+
 const CardComponent = {
     props: {
         "item": Object,
