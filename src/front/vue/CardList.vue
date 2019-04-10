0072974b9b3d5efc09f9919663576145d70db49d
Isaac Avram <avrisaac555@gmail.com>
2019-04-09 17:52:22 -0700

    Added card list component

--- src/front/vue/CardList.vue
+++ src/front/vue/CardList.vue
@@ -1,3 +1,35 @@
 <template>
-    
+    <main v-on:click="move" class="card-list">
+        <card v-for="i in items"
+              :item="i"
+              :key="i.id"
+              ref="cardList">
+        </card>
+    </main>
 </template>
+
+<script>
+import Card from "./Card.vue";
+
+export default {
+    data: function () {
+        return {
+            "activeItem": 0
+        };
+    },
+    props: ["items"],
+    components: {
+        "card": Card
+    },
+    methods: {
+        "move": function(itemNumber) {
+            const i = typeof(itemNumber) === "number" ? itemNumber : this.activeItem + 1;
+
+            this.activeItem = i;
+            setTimeout(() => this.$scrollTo(this.$refs.cardList[i].$el, {}), 300);
+
+            this.$root.setBackground(this.items[i].background);
+        }
+    }
+}
+</script>
