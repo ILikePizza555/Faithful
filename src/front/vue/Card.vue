28d3bc448e19d64e0a2126ffc1b8524e4ef32590
Isaac Avram <avrisaac555@gmail.com>
2019-04-09 17:37:18 -0700

    Added Card component

---
+++ src/front/vue/Card.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="card">
+        <h1>{{ item.title }}</h1>
+        <p v-if="item.desc">{{ item.desc }}</p>
+    </section>
+</template>
+
+<style lang="scss">
+@import '../sass/variables';
+
+.card {
+    display: block;
+    width: 100%;
+
+    height: 100vh - $cardpadding-top;
+    padding-top: $cardpadding-top
+}
+</style>
+
+<script>
+export default {
+    props: {
+        "item": Object
+    }
+};
+</script>
