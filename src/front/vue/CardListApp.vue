261f3a88fa6929c8851b1492194db1d37263b322
Isaac Avram <avrisaac555@gmail.com>
2019-04-10 15:17:36 -0700

    Added CardListApp.vue and common.scss

---
+++ src/front/vue/CardListApp.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app-card-list" class="full-width-container">
+        <div class="center">
+            <card-list :items="items"></card-list>
+        </div>
+</template>
+
+<style lang="scss">
+@import "../sass/common";
+
+#app-card-list {
+     transition: background-color 1s ease 0.3s, 
+                 color 1s ease 0.3s;
+}
+</style>
+
+<script>
+import CardList from "./CardList.vue";
+
+export default {
+    components: {
+        "card-list": CardList
+    }
+}
+</script>
