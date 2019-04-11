6da9e9f17d5f370dfcafd44eda3486f94c5f756d
Isaac Avram <avrisaac555@gmail.com>
2019-04-10 17:15:40 -0700

    Added a mount-point for the app

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import CardListApp from "../vue/CardListApp.vue";
 
 const vm = new Vue({
+    el: "#mount-point",
     components: {
         "card-list-app": CardListApp
     }
