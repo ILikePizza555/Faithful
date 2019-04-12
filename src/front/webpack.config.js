37edcb5158f4f46f0dd8320b72f20430d75303f4
Isaac Avram <avrisaac555@gmail.com>
2019-04-12 08:39:48 -0700

    Updated webpack config

--- src/front/webpack.config.js
+++ src/front/webpack.config.js
@@ -1,23 +1,25 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackCdnPlugin = require('webpack-cdn-plugin');
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackCdnPlugin = require("webpack-cdn-plugin");
 const path = require("path");
 
 module.exports = {
     context: __dirname,
     entry: path.join(__dirname, "js", "index.js"),
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: [".ts", ".js", ".vue"]
     },
     module: {
         rules: [
             {
                 test: /\.vue$/,
-                loader: 'vue-loader'
+                loader: "vue-loader"
             },
             {
                 test: /\.tsx?$/,
-                loader: 'ts-loader',
+                loader: "ts-loader",
                 exclude: /node_modules/,
                 options: {
                     appendTsSuffixTo: [/\.vue$/],
