c54454bef320e43dbb38bc2402f8e324c3500ab4
Isaac Avram <avrisaac555@gmail.com>
2019-04-12 10:50:38 -0700

    Added index.js

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,10 +1,38 @@
 import Vue from "vue";
+import {fromHex} from "./Color";
 import CardListApp from "../vue/CardListApp.vue";
+
+const data = {
+    "init": {
+        background: { color: fromHex("#fff") }
+    },
+    "items": [
+        {
+            id: 0,
+            title: "🚿 Take a shower",
+            background: { color: fromHex("#f0f8ff") }
+        },
+        {
+            id: 1,
+            title: "💊 Take medications",
+            background: { color: fromHex("#fff6f0") }
+        },
+        {
+            id: 2,
+            title: "👗 Wear something pretty~",
+            background: {color: fromHex("#ff4f4f") }
+        }
+    ]
+};
 
 const vm = new Vue({
     el: "#mount-point",
+    data: data,
     components: {
         "card-list-app": CardListApp
     },
-    template: "<card-list-app></card-list-app>"
+    template: `<card-list-app 
+        :initialBackground='init.background'
+        :items='items'>
+    </card-list-app>`
 });
