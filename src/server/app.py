bf1ff419a06073ab0c0236cae592fec220e827fb
Isaac Avram <avrisaac555@gmail.com>
2019-04-16 13:33:33 -0700

    Added config loading and index route to app.py

---
+++ src/server/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from sassutils.wsgi import SassMiddleware
+import json
+
+with open("server_confg.json", mode="r") as f:
+    CONFIG = json.load(f)
+
+def template_context_from_config(config=CONFIG):
+    return {
+        "google_clid": CONFIG["google"]["client_id"]
+    }
+
+app = Flask(__name__)
+
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    "app": ("static/scss", "static/css", "/static/css")
+})
+
+@app.route("/")
+def index_route():
+    return render_template("index.html", **template_context_from_config())
