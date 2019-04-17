e655a3fcf6e33252908b7a397b133f6f8ed01dc0
Isaac Avram <avrisaac555@gmail.com>
2019-04-17 15:35:26 -0700

    Added firebase to app.py

--- src/server/app.py
+++ src/server/app.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template
 from sassutils.wsgi import SassMiddleware
 import json
+import firebase_admin
+from firebase_admin import credentials
 
 with open("server_conf.json", mode="r") as f:
     CONFIG = json.load(f)
 
-def template_context_from_config(config=CONFIG):
-    return {
-        "google_clid": CONFIG["google"]["client_id"]
-    }
-
+# Flask config
 app = Flask(__name__)
-
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
     "app": ("static/scss", "static/css", "/static/css")
 })
 
+# Firebase config
+cred = credentials.Certificate(CONFIG["firebase_service_key_path"])
+firebase_admin.initialize_app(cred)
+
 @app.route("/")
 def index_route():
-    return render_template("index.html", **template_context_from_config())
+    return render_template("index.html")
 
-@app.route("/tokensignin")
+@app.route("/oauth2signin")
 def signin_route():
     pass
 
