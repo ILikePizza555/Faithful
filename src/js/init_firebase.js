6d46dd17a029012b56da90848a24aa6ae860fb15
Isaac Avram <avrisaac555@gmail.com>
2019-04-18 09:34:21 -0700

    Added firebase init to its own js file

---
+++ src/js/init_firebase.js
@@ -0,0 +1,17 @@
+/**
+ * Script that imports and initializes the Firebase library
+ */
+import * as firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDDCVGzxZb1cM-Be3sRtbGFdhRP2-AHMUE",
+    authDomain: "faithful.firebaseapp.com",
+    databaseURL: "https://faithful.firebaseio.com",
+    projectId: "faithful",
+    storageBucket: "faithful.appspot.com",
+    messagingSenderId: "32800656929"
+};
+firebase.initializeApp(firebaseConfig);
