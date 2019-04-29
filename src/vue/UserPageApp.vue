1569d375733a25bc81a8b8c2e595f3a8288fed0c
Isaac Avram <avrisaac555@gmail.com>
2019-04-29 15:59:03 -0700

    Added UserPageApp component

---
+++ src/vue/UserPageApp.vue
@@ -0,0 +1,27 @@
+<!-- Component that's displayed on the root route. Renders the user page. -->
+<template>
+    <div id="user-page">
+        <h1>Hello {{displayName}}.</h1>
+        <h2>What are we doing today?</h2>
+        <div id="tl-list">
+            <a v-for="i in items"
+                 v-bind:key="i.id"
+                 class="list-item">
+                {{i.name}}
+            </a>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import {Prop} from "vue/types/options";
+import {TodoList} from "../js/Models"
+
+export default Vue.extend({
+    props: {
+        "displayName": String,
+        "items": Array as Prop<TodoList[]>
+    }
+})
+</script>
