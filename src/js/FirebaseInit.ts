a4328b7914a794a2ee1feeaf0788de119ca3d3f4
Isaac Avram <avrisaac555@gmail.com>
2019-04-30 15:44:13 -0700

    Removed the default export

--- src/js/FirebaseInit.ts
+++ src/js/FirebaseInit.ts
@@ -6,7 +6,7 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-export default {firebase}
+export {firebase}
 
 const firebaseConfig = {
     apiKey: "AIzaSyDDCVGzxZb1cM-Be3sRtbGFdhRP2-AHMUE",
