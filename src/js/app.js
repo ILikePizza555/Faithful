6fe91894bbc9396049aaa58e93111d590c9a6a49
Isaac Avram <avrisaac555@gmail.com>
2019-04-30 14:15:49 -0700

    Added firebase loading

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 /* eslint-env browser */
-import {firebase, fsDb} from "./init_firebase";
+import {firebase} from "./FirebaseInit";
 
 import Vue from "vue";
 import VueRouter from "vue-router";
@@ -7,15 +7,42 @@ import VueRouter from "vue-router";
 import CardListApp from "../vue/CardListApp.vue";
 import UserPageApp from "../vue/UserPageApp.vue";
 
+import {TodoList, Collections, getCollectionRef} from "./Models";
+
 Vue.use(VueRouter);
+
+const vmData = {
+    userInfo: null,
+    lists: []
+}
 
 const router = new VueRouter({
     routes: [
-        {path: "/app", component: UserPageApp},
-        {path: "/app/list/:id", component: CardListApp}
+        {path: "/", component: UserPageApp, params: true},
+        {path: "/list/:id", component: CardListApp}
     ]
 })
 
 const vm = new Vue({
-    router
-}).$mount("#mount-point");
+    router,
+    data: vmData,
+    template: "<router-view></router-view>"
+});
+
+firebase.auth().onAuthStateChanged(function authStateHandler(user) {
+    if(user) {
+        // User is logged in. 
+        vmData.userInfo = user;
+
+        // Get user's lists from database
+        TodoList.getByOwner(getCollectionRef(Collections.LISTS), user.uid)
+            .then(tls => {vmData.lists = tls})
+            .catch(err => {console.error("An error occured while accessing database: " + err)})
+
+        if(!vm.$el) {
+            vm.$mount("#mount-point");
+        }
+    } else {
+        vmData.userInfo = null;
+    }
+})
