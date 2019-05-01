9b84fff71ae57be0c9344ac35f0f068f77503245
Isaac Avram <avrisaac555@gmail.com>
2019-04-30 18:14:26 -0700

    Revert "CardListApp now uses a class-based component"
    This reverts commit a99aaf183bc4e0f7019323329a069a1acd2b2893.

--- src/vue/CardListApp.vue
+++ src/vue/CardListApp.vue
@@ -5,7 +5,7 @@
         <div class="center">
             <card-list
                 v-on:item-update="itemUpdateHandler($event)"
-                :items="todoListModel.items"></card-list>
+                :items="items"></card-list>
         </div>
     </div>
 </template>
@@ -20,32 +20,42 @@
 </style>
 
 <script lang="ts">
-import {Vue, Component, Prop } from "vue-property-decorator";
-
-import {Background, TodoList} from "../js/Models";
+import Vue from 'vue'
+import {Prop} from "vue/types/options";
+import {Background, Item} from "../js/Models";
 import CardList from "./CardList.vue";
 
-@Component({
+type DataType = {
+    background: Background
+}
+
+export default Vue.extend({
+    data: function(): DataType {
+        return {
+            background: this.initialBackground
+        }
+    },
+    props: {
+        items: Array as Prop<Item[]>,
+        initialBackground: Object as Prop<Background>
+    },
+    methods: {
+        itemUpdateHandler: function(itemIndex: number) {
+            this.background = this.items[itemIndex].background;
+        }
+    },
+    computed: {
+        fontColor: function(): string {
+            const bgObj: Background = this.background;
+
+            if(bgObj.color.luminance > (150 / 255)) {
+                return "#000000";
+            }
+            return "#FFFFFF";
+        }
+    },
     components: {
         "card-list": CardList
     }
-})
-export default class CardListApp extends Vue {
-    @Prop({type: TodoList, required: true}) todoListModel!: TodoList;
-
-    background: Background = this.todoListModel.items[0].background;
-
-    public itemUpdateHandler(itemIndex: number) {
-        this.background = this.todoListModel.items[itemIndex].background;
-    }
-
-    get fontColor(): string {
-        const bgObj: Background = this.background as Background;
-
-        if(bgObj.color.luminance > (150 / 255)) {
-            return "#000000";
-        }
-        return "#FFFFFF";
-    }
-}
+});
 </script>
