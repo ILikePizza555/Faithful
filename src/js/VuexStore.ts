57da821ae3960f9cc10f84aeba2bd44ba18780f2
Isaac Avram <avrisaac555@gmail.com>
2019-05-02 16:09:28 -0700

    added todoLists to StoreState and init mutation

--- src/js/VuexStore.ts
+++ src/js/VuexStore.ts
@@ -1,17 +1,24 @@
 import Vue from "vue";
-import Vuex, { StoreOptions } from "vuex";
+import Vuex from "vuex";
+import { TodoList } from "./Models";
 
 Vue.use(Vuex);
 
 interface StoreState {
     userInfo: firebase.User | null;
+    userTdLists: TodoList[];
 }
 
 export const store = new Vuex.Store<StoreState>({
     state: {
-        userInfo: null
+        userInfo: null,
+        userTdLists: []
     },
     mutations: {
+        initState(state, init) {
+            state.userInfo = init.userInfo;
+            state.userTdLists = init.userTdLists;
+        },
         userUpdate(state, user) {
             state.userInfo = user;
         }
