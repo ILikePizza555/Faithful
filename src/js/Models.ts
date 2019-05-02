87328c40b72cef61fdcb8783bc51812356c258a6
Isaac Avram <avrisaac555@gmail.com>
2019-05-02 15:55:06 -0700

    Updated models to use less computing resources

--- src/js/Models.ts
+++ src/js/Models.ts
@@ -1,4 +1,4 @@
-import {firebase, firestore} from "./Firebase";
+import {firebase, firestore, Timestamp, DocumentSnap} from "./Firebase";
 import {Color, fromHex} from "./Color";
 
 export interface Background {
@@ -12,47 +12,38 @@ export interface Item {
 }
 
 /**
- * An entry in "items" as defined in Firestore
+ * Item as defined in Firestore.
  */
-interface ItemSchema {
+interface FirestoreItem {
     id: number;
     title: string;
     background: {
         color: string;
     };
 }
+type AugmentedItem = {_background: {color: string}} & Item;
+/**
+ * Builds an object of type Item from Firestore data.
+ * The `background` property is lazily evaluated to save on computing resources.
+ * @param fsObj 
+ */
+function itemFromFS(fsObj: FirestoreItem): AugmentedItem {
+    return {
+        ...fsObj,
+        _background: fsObj.background,
+        get background(): Background {
+            return {
+                color: fromHex(this._background.color)
+            }
+        }
+    }
+}
 
 /**
  * Wrapper around a DocumentSnapshot that makes accessing fields easier.
  */
 export class TodoList {
-    /**
-     * Queries the database for all documents containing an "owner" field that matches the uid,
-     * and returns them as an array of TodoLists.
-     * 
-     * @param collectionRef A reference to a collection to query.
-     * @param uid The uid to match against
-     */
-    public static getByOwner(collectionRef: CollectionRef, uid: string): Promise<TodoList[]> {
-        return collectionRef
-            .where("owner", "==", uid)
-            .get()
-            .then(q => q.docs.map(doc => new TodoList(doc)))
-    }
-
-    /**
-     * Queries the database for a single document matching the given id.
-     * @param collectionRef 
-     * @param id 
-     */
-    public static getById(collectionRef: CollectionRef, id: string): Promise<TodoList> {
-        return collectionRef
-            .doc(id)
-            .get()
-            .then(q => new TodoList(q));
-    }
-
-    public constructor(private _document: DocumentSnapshot) {
+    public constructor(private _document: DocumentSnap) {
     }
 
     public get id(): string {return this._document.id}
@@ -61,15 +52,13 @@ export class TodoList {
     public get dateCreated(): Timestamp {return this._document.get("created_date")}
 
     public get items(): Item[] {
-        //TODO: Cache results instead of generating them every time
         return this._document
             .get("items")
-            .map((i: ItemSchema): Item => {
-                const colorObj = fromHex(i.background.color);
-                return {...i, background: {color: colorObj}};
-            });
+            .map(itemFromFS);
     }
 
-    //TODO: Implement setters
-    //TODO: Figure out if model should automatically update with onSnapshot
+    public item(index: number): Item {
+        const fbObj: FirestoreItem = this._document.get("items")[index];
+        return itemFromFS(fbObj);
+    }
 }
