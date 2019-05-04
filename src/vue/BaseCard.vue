ca695384bcb48fa727e19f140e711aee702c4286
Isaac Avram <avrisaac555@gmail.com>
2019-05-03 19:44:20 -0700

    Moved card scss to separate file

--- src/vue/BaseCard.vue
+++ src/vue/BaseCard.vue
@@ -6,27 +6,7 @@
 </template>
 
 <style lang="scss">
-@import '../sass/variables';
-
-.card {
-    display: block;
-    width: 100%;
-
-    height: 100vh - $cardpadding-top;
-    padding-top: $cardpadding-top;
-
-    opacity: 1;
-    line-height: 1.2;
-    font-size: 16px;
-}
-
-.card h1 {
-    font-size: 3rem;
-}
-
-.card.done {
-    opacity: 0.3 !important;
-}
+@import "../sass/card";
 </style>
 
 <script>
