d2bd09ccf3e21efe3bd4ab9dd76a7798fcdf1ed5
Isaac Avram <avrisaac555@gmail.com>
2019-05-03 18:56:00 -0700

    Added EditableCard

---
+++ src/vue/EditableCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="card styled-inputs">
+        <h1>
+            <input v-bind:value="value.title"
+                   v-on:input="$emit('input', {...value, title: $event.target.value})"
+                   type="text">
+        </h1>
+        <input v-bind:value="value.desc"
+               v-on:input="$emit('input', {...value, desc: $event.target.value})"
+               type="text">
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: ["value"]
+})
+</script>
