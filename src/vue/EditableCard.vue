39937770860f7e64c19f4ff0f4dfa7e93ae31cdd
Isaac Avram <avrisaac555@gmail.com>
2019-05-05 13:46:49 -0700

    Refactored template code in EditableCard

--- src/vue/EditableCard.vue
+++ src/vue/EditableCard.vue
@@ -1,17 +1,21 @@
 <template>
     <section class="card styled-inputs">
-        <h1>
-            <input v-if="editing"
-                   v-bind:value="value.title"
-                   v-on:input="$emit('input', {...value, title: $event.target.value})"
-                   type="text">
-            <span v-else>{{value.title}}</span>
-        </h1>
-        <input v-if="editing"
-               v-bind:value="value.desc"
-               v-on:input="$emit('input', {...value, desc: $event.target.value})"
-               type="text">
-        <span v-else>{{value.desc}}</span>
+        <template v-if="editing">
+            <h1>
+                <input
+                    :value="value.title"
+                    @input="$emit(EditTitleEvent, {value, edit: $event.target.value})"
+                    type="text">
+            </h1>
+            <input
+                :value="value.desc"
+                @input="$emit(EditDescEvent, {value, edit: $event.target.value})"
+                type="text">
+        </template>
+        <template v-else>
+            <h1>{{value.title}}</h1>
+            <span>{{value.desc}}</span>
+        </template>
     </section>
 </template>
 
@@ -24,6 +28,9 @@
 <script lang="ts">
 import Vue from 'vue'
 import BaseCard from "./BaseCard.vue";
+
+export const EditTitleEvent = "edit-title";
+export const EditDescEvent = "edit-desc";
 
 export default Vue.extend({
     extends: BaseCard,
