f1c0a0c01c3ea03a87d7db0d8f5a0fabca269c7c
Isaac Avram <avrisaac555@gmail.com>
2019-05-05 23:34:23 -0700

    Revert "Added BaseDocument class"
    This reverts commit e6d3ac36beded53b9b234f80ec9f77f1aa5a3a9e.

--- src/js/Models.ts
+++ src/js/Models.ts
@@ -1,4 +1,4 @@
-import {firebase, firestore, Timestamp, DocumentSnap, CollectionRef, DocumentRef} from "./Firebase";
+import {firebase, firestore, Timestamp, DocumentSnap, CollectionRef} from "./Firebase";
 import {Color, fromHex} from "./Color";
 
 export interface Background {
@@ -40,66 +40,11 @@ function itemFromFS(fsObj: FirestoreItem): AugmentedItem {
 }
 
 /**
- * Base class for models of Firebase Documents.
- * 
- * The purpose of this class is to make it easier to deal with document changes by automatically
- * setting up the appropriate listeners.
- * 
- * Child classes should have property accessesors through the use of getters.
- */
-export abstract class BaseDocument {
-    protected abstract readonly _tag: string;
-    protected readonly _docRef: DocumentRef;
-    protected readonly _unsubscribe: () => void;
-    protected _docSnap!: DocumentSnap;
-
-    protected constructor(docRef: DocumentRef) {
-        this._docRef = docRef;
-
-        //Setup snapshot listeners
-        this._unsubscribe = this._docRef.onSnapshot(
-            this._snapshotCallback,
-            this._snapshotErrorCallback
-        );
-
-        this._docRef.get().then(this._snapshotCallback);
-    }
-
-    /**
-     * Callback passed to DocumentReference:onSnapshot. The main purpose
-     * of this method is to delegate to onLocalSnapshot and onServerSnapshot.
-     * @param doc 
-     */
-    protected _snapshotCallback(doc: DocumentSnap): void {
-        doc.metadata.hasPendingWrites ? 
-            this._onLocalSnapshot(doc) : 
-            this._onServerSnapshot(doc);
-    }
-
-    /** 
-     * Callback which is invoked when a document was changed locally,
-     * but not yet written to the server.
-     */
-    protected _onLocalSnapshot(doc: DocumentSnap): void {}
-
-    /**
-     * Callback invoked when a document was changed on the server.
-     * @param doc 
-     */
-    protected _onServerSnapshot(doc: DocumentSnap): void {
-        this._docSnap = doc;
-    }
-
-    protected _snapshotErrorCallback(err: Error): void {
-        console.error("[BaseDocument:" + this._tag +"]" + err);
-    }
-}
-
-
-/**
  * Wrapper around a DocumentSnapshot that makes accessing fields easier.
  */
-export class TodoListDocument {
+export class TodoList {
+    private readonly _tag = "model_tdlist";
+
     /**
      * Queries the given collection for all documents which have an `owner` field that
      * matches the given ownerUid. After which, the documents are converted into `TodoList`
@@ -108,15 +53,12 @@ export class TodoListDocument {
      * @param collection 
      */
     public static getAllByOwner(ownerUid: string,
-        collection: CollectionRef=firestore.listCollectionRef): Promise<TodoListDocument[]> {
+        collection: CollectionRef=firestore.listCollectionRef): Promise<TodoList[]> {
         
         return collection.where("owner", "==", ownerUid)
             .get()
-            .then(q => q.docs.map(d => new TodoListDocument(d)))
+            .then(q => q.docs.map(d => new TodoList(d)))
     }
-
-    private readonly _tag = "model_tdlist";
-    private readonly _docRef;
 
     public constructor(private _document: DocumentSnap) {
     }
