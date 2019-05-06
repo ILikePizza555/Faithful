74eaf1bfe2bfedef5dc766a27c022ef8dbf5a198
Isaac Avram <avrisaac555@gmail.com>
2019-05-06 13:59:13 -0700

    Added functions to create Observables

--- src/js/Firebase.ts
+++ src/js/Firebase.ts
@@ -6,6 +6,8 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+import {Observable} from "rxjs";
+
 export {firebase}
 
 const firebaseConfig = {
@@ -18,16 +20,44 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-export type Timestamp = firebase.firestore.Timestamp;
-export type CollectionRef = firebase.firestore.CollectionReference;
-export type DocumentRef = firebase.firestore.DocumentReference;
-export type DocumentSnap = firebase.firestore.DocumentSnapshot;
-
-interface AugFirestore extends firebase.firestore.Firestore {
-    listCollectionRef: firebase.firestore.CollectionReference;
-    userinfoCollectionRef: firebase.firestore.CollectionReference;
+/**
+ * Creates an Observable from firebase.auth().onAuthStateChanged.
+ */
+export function createAuthStateObservable(): Observable<firebase.User | null> {
+    return new Observable(subscriber => firebase.auth().onAuthStateChanged(
+        subscriber.next,
+        subscriber.error,
+        subscriber.complete
+    ));
 }
 
-export const firestore: AugFirestore = firebase.firestore() as AugFirestore;
-firestore.listCollectionRef = firestore.collection("lists");
-firestore.userinfoCollectionRef = firestore.collection("user_info");
+export type FTimestamp = firebase.firestore.Timestamp;
+export type FCollectionRef = firebase.firestore.CollectionReference;
+export type FDocumentRef = firebase.firestore.DocumentReference;
+export type FDocumentSnap = firebase.firestore.DocumentSnapshot;
+
+export const firestore = firebase.firestore();
+export const collections = {
+    lists: firestore.collection("lists"),
+    userInfo: firestore.collection("user_info")
+}
+
+interface OnSnapshot<T> {
+    onSnapshot(observer: {
+        complete?: () => void; 
+        error?: (err: firebase.firestore.FirestoreError) => void;
+        next?: (snapshot: T) => void;
+    }): () => void;
+    onSnapshot(
+        onNext?: (snapshot: T) => void, 
+        onError?: (err: firebase.firestore.FirestoreError) => void,
+        onCompletetion?: () => void): () => void;
+}
+
+/**
+ * Creates an Observable by calling onSnapshot on the providec object.
+ * @param obj 
+ */
+export function createSnapshotObservable<T>(obj: OnSnapshot<T>): Observable<T> {
+    return new Observable(obj.onSnapshot);
+}
