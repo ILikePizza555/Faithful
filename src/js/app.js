6a2c74cc4e58aa06db6c0fafd241fee98a950e36
Isaac Avram <avrisaac555@gmail.com>
2019-05-06 20:19:05 -0700

    Revert "Switched to observables to handle server state"
    This reverts commit 69051d7a5a8385d11730ab8951ed2c6900179993.

--- src/js/app.js
+++ src/js/app.js
@@ -1,56 +1,25 @@
 /* eslint-env browser */
-import {createAuthStateObservable, createSnapshotObservable, collections} from "./Firebase";
+import {firebase} from "./Firebase";
 
 import Vue from "vue";
 import {createRouter} from "./Routes";
 import {store} from "./VuexStore";
 
-import {mergeMap, takeUntil, filter, zip, first} from "rxjs/operators";
+import {TodoList} from "./Models";
 
 const vm = new Vue({
     router: createRouter(),
+    el: "#mount-point",
     store,
     template: "<router-view></router-view>"
 });
 
-const authStateObservable = createAuthStateObservable();
-
-// Each time the auth state changes, we need to update the snapshot listener.
-// This is easily done with flatMap, but a takeUntil needs to be attached so that
-// memory isn't leaked.
-const snapshotObservable = authStateObservable.pipe(
-    mergeMap(user => 
-        createSnapshotObservable(collections.lists.where("owner", "==", user.uid)))
-        .pipe(
-            takeUntil(authStateObservable.pipe(filter(u => u === null)))
-        )
-)
-
-const initObservable = zip(authStateObservable, snapshotObservable).pipe(first());
-
-authStateObservable.subscribe({
-    next: function(user) {
-        store.commit("userUpdate", user);
-    },
-    error: function(err) {
-        console.error("[authStateObservable]" + err);
+firebase.auth().onAuthStateChanged(function authStateHandler(user) {
+    if(user) {
+        // User is logged in. 
+        // Get user's lists from database
+        TodoList.getAllByOwner(user.uid)
+            .then(tls => store.commit("initState", {userInfo: user, userTdLists: tls}))
+            .catch(err => {console.error("An error occured while accessing database: " + err)})
     }
-});
-
-snapshotObservable.subscribe({
-    next: function(q) {
-        //Do nothing on local snapshots
-        if(q.metadata.hasPendingWrites) {return;}
-
-        //Commit changes to the store.
-        q.docChanges.forEach(docChange => {
-            store.commit("listServerDocChange", docChange);
-        });
-    },
-    error: function(err) {
-        console.error("[snapshotObservable]" + err);
-    }
-});
-
-// Once each callback has fired once, mount Vue.
-initObservable.subscribe(v => vm.$mount("#mount-point"));
+})
