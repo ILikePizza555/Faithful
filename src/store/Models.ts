3cd219060e1621005387561a3c150ea091f031d6
Izzy! <izzy@MacBook-Pro.midnighthack.tech>
2019-05-08 13:34:48 -0700

    Added modification types to Models

--- src/store/Models.ts
+++ src/store/Models.ts
@@ -1,12 +1,47 @@
 import {FTimestamp, FDocumentSnap} from "../js/Firebase";
 
-export interface TodoListItem {
-    readonly id: number;
-    title: string;
-    desc?: string;
-    background: {
-        color: string;
-    };
+export namespace TodoListItem {
+    export interface TodoListItem {
+        readonly id: number;
+        title: string;
+        desc?: string;
+        background: {
+            color: string;
+        };
+    }
+
+    /**
+     * Represents the addition of a new TodoListItem to a TodoListDocument.
+     */
+    export interface AddModification {
+        readonly todoListId: string;
+        readonly itemId: -1;
+        readonly type: "add";
+        change: TodoListItem;
+    }
+
+    /**
+     * Represents the removal of a TodoListItem from a TodoListDocument.
+     */
+    export interface RemoveModification {
+        readonly todoListId: string;
+        readonly itemId: number;
+        readonly type: "remove";
+    }
+
+    /** Represents an edit to a TodoListItem */
+    export interface ChangeModifications {
+        readonly todoListId: string;
+        readonly itemId: number;
+        readonly type: "change";
+        change: {
+            title?: string;
+            desc?: string;
+            background?: any;
+        }
+    }
+
+    export type Modification = AddModification | RemoveModification | ChangeModifications;
 }
 
 export class TodoListDocument {
@@ -17,7 +52,7 @@ export class TodoListDocument {
     public readonly dateCreated: FTimestamp;
 
     public name: string;
-    public items: TodoListItem[];
+    public items: TodoListItem.TodoListItem[];
 
     public constructor(private _document: FDocumentSnap) {
         this.id = _document.id;
