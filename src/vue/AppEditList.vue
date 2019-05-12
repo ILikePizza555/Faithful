cbb4fdb318bd0549829a48d6e20965b108c8b464
Izzy! <izzy@Izzy-Luna.local>
2019-05-12 14:51:43 -0700

    Template code to AppEditList

---
+++ src/vue/AppEditList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app-card-editor"
+         class="full-page-container">
+        <app-bar>
+        </app-bar>
+    </div>
+</template>
+
+<style lang="scss">
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+import CardList, {UpdateActiveItemEvent} from "./CardList.vue";
+import TheAppBar from "./TheAppBar.vue";
+
+export default Vue.extend({
+    props: {
+        id: {
+            type: String,
+            required: true
+        }
+    },
+    components: {
+        'app-bar': TheAppBar,
+        'card-list': CardList
+    }
+});
+</script>
