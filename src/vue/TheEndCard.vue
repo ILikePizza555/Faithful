1b707afb3f38750b43e96ceab314257cd8934a2a
Izzy <avrisaac555@gmail.com>
2019-06-03 15:44:03 -0700

    Added TheEndCard

---
+++ src/vue/TheEndCard.vue
@@ -0,0 +1,30 @@
+<!-- The last card in a CardList. Either displays a congratulatory message, or, if editing,
+a button which adds a new entry into the list. -->
+<template>
+    <div class="card-container viewport">
+        <section class="card">
+            <button
+                @click="$emit('newItemClick')"
+                v-if="editing">
+                <i class="material-icons">add</i>
+                Add new item
+            </button>
+            <p v-else>
+                🎉 You've completed everything! Good job!
+            </p>
+        </section>
+    </div>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: {
+        "editing": {
+            type: Boolean,
+            default: false
+        }
+    }
+})
+</script>
