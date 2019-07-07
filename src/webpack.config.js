13a10c246531204f96225c8d1ca631f1c4044927
Izzy <avrisaac555@gmail.com>
2019-07-07 16:56:24 -0700

    Updated webpack config

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -6,15 +6,9 @@ const path = require("path");
 
 module.exports = {
     context: __dirname,
-    entry: {
-        app: path.join(__dirname, "js", "app.js"),
-        index: path.join(__dirname, "js", "index.js")
-    },
+    entry: path.join(__dirname, "js", "index.js"),
     resolve: {
-        extensions: [".ts", ".js", ".vue"],
-        alias: {
-            "vue$": "vue/dist/vue.esm.js"
-        }
+        extensions: [".ts", ".js", ".vue"]
     },
     module: {
         rules: [
@@ -25,7 +19,6 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 loader: "ts-loader",
-                exclude: /node_modules/,
                 options: {
                     appendTsSuffixTo: [/\.vue$/],
                 }
@@ -51,37 +44,18 @@ module.exports = {
     plugins: [
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
-            filename: "app.html",
-            template: path.resolve("src", "html", "app.pug"),
-            chunks: ["app"],
+            filename: "index.html",
+            template: path.resolve("src", "html", "index.pug"),
             title: "Faithful",
             styles: [
                 "https://unpkg.com/normalize.css@8.0.1/normalize.css",
-                "https://fonts.googleapis.com/icon?family=Material+Icons"
+                "https://fonts.googleapis.com/icon?family=Material+Icons",
+                "https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css"
             ],
             scripts: [
                 "https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
                 "https://unpkg.com/vuex@3.1.1/dist/vuex.js",
                 "https://unpkg.com/vue-router/dist/vue-router.js",
-                "https://www.gstatic.com/firebasejs/6.1.0/firebase-app.js",
-                "https://www.gstatic.com/firebasejs/5.10.1/firebase-auth.js",
-                "https://www.gstatic.com/firebasejs/5.10.1/firebase-firestore.js"
-            ],
-            meta: {
-                "viewport": "width=device-width, initial-scale=1, shrink-to-fit=no",
-                "charset": "utf-8"
-            }
-        }),
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: path.resolve("src", "html", "index.pug"),
-            chunks: ["index"],
-            title: "Faithful",
-            styles: [
-                "https://unpkg.com/normalize.css@8.0.1/normalize.css",
-                "https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css"
-            ],
-            scripts: [
                 "https://www.gstatic.com/firebasejs/6.1.0/firebase-app.js",
                 "https://www.gstatic.com/firebasejs/6.1.0/firebase-auth.js",
                 "https://www.gstatic.com/firebasejs/6.1.0/firebase-firestore.js",
