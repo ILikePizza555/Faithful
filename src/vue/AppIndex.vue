ad7e528e8fdb2a13fe8eb1782fe37f6ceaf1a35a
Izzy <avrisaac555@gmail.com>
2019-07-08 10:06:23 -0700

    AppIndex markup has new root

--- src/vue/AppIndex.vue
+++ src/vue/AppIndex.vue
@@ -1,20 +1,20 @@
 <!-- Main page of the app. Mostly static. Shows some information and displays a login prompt -->
 <template lang="pug">
-    header.index-header.block-container
-        div.info-block
-            h1 It's time to get things done.
-            p Build habits and manage your life with Faithful.
-        div.login-block
-            p Let's get started:
-            div#firebaseui-auth-container
+    div#index-app
+        header.index-header.block-container
+            div.info-block
+                h1 It's time to get things done.
+                p Build habits and manage your life with Faithful.
+            div.login-block
+                p Let's get started:
+                div#firebaseui-auth-container
 </template>
+
+<style lang="scss">
+</style>
 
 <script>
 export default {
 
 }
 </script>
-
-<style>
-
-</style>
