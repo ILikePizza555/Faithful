076d6c2b581a2d7a76af37c76c0e5143f26d7e06
Izzy <avrisaac555@gmail.com>
2019-07-17 11:18:10 -0700

    Updated router mode and added catch-all route

--- src/script/Routes.ts
+++ src/script/Routes.ts
@@ -15,7 +15,8 @@ export const routes = [
     {path: "/", component: AppIndex},
     {path: "/user", name: "userPage", component: AppUserPage},
     {path: "/list/:id", name: "list", component: AppCardList, props: true},
-    {path: "/list/edit/:id", name: "editList", component: AppEditList, props:true}
+    {path: "/list/edit/:id", name: "editList", component: AppEditList, props:true},
+    {path: "*", redirect: "userPage"}
 ];
 
 /**
@@ -24,6 +25,7 @@ export const routes = [
  */
 export function createRouter(options?: RouterOptions): VueRouter {
     return new VueRouter({
+        mode: "history",
         ...options,
         routes
     });
