d300b923981d8903f9cb450c6b1dbafa65ccce69
Izzy <avrisaac555@gmail.com>
2019-07-17 16:27:30 -0700

    Added UserProfile model

--- src/store/Models.ts
+++ src/store/Models.ts
@@ -89,3 +89,16 @@ export class TodoListDocument {
         });
     }
 }
+
+export class UserProfile {
+    readonly _tag = "model_UserData";
+
+    public preferredName?: string;
+
+    protected constructor() {
+    }
+
+    static constructEmpty() {
+        return new UserProfile();
+    }
+}
